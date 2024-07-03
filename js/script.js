7b883dedc896fa9fb5cfcd226733f23a22324c62
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2024-07-03 06:45:07 +0330

    Dashboard_Finance_Bank_Fintechwebsite

---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    const sidebar = document.querySelector(".sidebar");
+    const closeBtn = document.querySelector("#btn");
+    const searchBtn = document.querySelector(".bx-search")
+    sidebar.classList.toggle("open")
+    menuBtnChange()
+
+    searchBtn.addEventListener("click",function(){
+        sidebar.classList.toggle("open")
+        menuBtnChange()
+    })
+
+    function menuBtnChange(){
+        if(sidebar.classList.contains("open")){
+            closeBtn.classList.replace("bx-menu","bx-menu-alt-right")
+        }else{
+            closeBtn.classList.replace("bx-menu-alt-right","bx-menu")
+        }
+    }
+}
